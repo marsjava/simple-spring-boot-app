f8540c1c65e55a3480a1ecb8a3954dd3f19cff69
marsjava <marikannan.cvp@gmail.com>
2020-07-10 01:28:31 +0530

    Initial commit.

---
+++ O1-spring-boot-app/src/main/java/com/learning/basic/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.learning.basic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/status")
+	public String getStatus() {
+		return "Say, Hello world.";
+	}
+}
