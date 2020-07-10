b7c5f6ee1587bbdc962d16d0758711dac682f36d
marsjava <marikannan.cvp@gmail.com>
2020-07-10 10:55:22 +0530

    Initial commit on devtools app

---
+++ O2-dev-tools-app/src/test/java/com/learning/basic/O1SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O1SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
